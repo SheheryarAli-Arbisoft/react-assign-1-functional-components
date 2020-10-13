314ddf59e0d840bc54d1688ac2c6eda634e35701
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-13 14:11:32 +0500

    Added jest and enzyme and created the tests for action creators

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -5,12 +5,12 @@ export const loadVideo = id => ({
   payload: id,
 });
 
-export const loadAllRelatedVideos = id => ({
-  type: LOAD_ALL_RELATED_VIDEOS,
-  payload: id,
-});
-
 export const loadAllVideos = description => ({
   type: LOAD_ALL_VIDEOS,
   payload: description,
+});
+
+export const loadAllRelatedVideos = id => ({
+  type: LOAD_ALL_RELATED_VIDEOS,
+  payload: id,
 });
--- src/components/Text/styled.js
+++ src/components/Text/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const Text = styled.div`
+export const Text = styled.div`
   display: ${({ theme, variant }) => theme.text.getDisplay(variant)};
   justify-content: ${({ variant }) =>
     variant === 'heading' ? 'center' : 'none'};
@@ -20,48 +20,3 @@ const Text = styled.div`
       small ? theme.spacing.small : theme.spacing.default};
   }
 `;
-
-const Heading = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: ${({ theme }) => theme.size.text.heading};
-  margin-bottom: ${({ theme }) => theme.spacing.xlarge};
-  font-weight: bold;
-  color: #ff0000;
-
-  & > i {
-    margin-right: ${({ theme }) => theme.spacing.default};
-  }
-`;
-
-const Title = styled.div`
-  color: ${({ theme }) => theme.colors.primaryTextColor};
-  font-size: ${({ theme, small }) =>
-    small ? theme.size.text.default : theme.size.text.large};
-  font-weight: bold;
-  margin-bottom: ${({ theme, small }) =>
-    small ? theme.spacing.small : theme.spacing.medium};
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
-const SubTitle = styled.div`
-  color: ${({ theme }) => theme.colors.secondaryTextColor};
-  font-size: ${({ theme, small }) =>
-    small ? theme.size.text.small : theme.size.text.medium};
-  margin-bottom: ${({ theme, small }) =>
-    small ? theme.spacing.small : theme.spacing.medium};
-`;
-
-const Description = styled.div`
-  font-size: ${({ theme, small }) =>
-    small ? theme.size.text.small : theme.size.text.medium};
-  color: ${({ theme }) => theme.colors.primaryTextColor};
-  white-space: ${({ full }) => (full ? 'none' : 'nowrap')};
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
-export { Heading, Title, SubTitle, Description, Text };
